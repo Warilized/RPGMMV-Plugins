63665f64aeb745d2ea424e7b8bbc601c6a21e0fc
Warilized <anim3ddicted@gmail.com>
2021-07-14 20:33:39 +0700

    update eqD_variableArray.js

--- eqD_variableArray/eqD_variableArray.js
+++ eqD_variableArray/eqD_variableArray.js
@@ -38,8 +38,8 @@ equalDelight.variableArray.version = "0.0.1";
  * @help
  * Variable Array by equalDelight
  * Version 0.0.1
- * Patch Notes: 
- * https://github.com/Warilized/RPGMMV-Plugins/blob/main/eqD_variableArray/PATCHNOTES.txt
+ * Release Notes: 
+ * https://github.com/Warilized/RPGMMV-Plugins/blob/main/eqD_variableArray/RELEASENOTES.txt
  *
  * ============================================================================
  * Introduction
