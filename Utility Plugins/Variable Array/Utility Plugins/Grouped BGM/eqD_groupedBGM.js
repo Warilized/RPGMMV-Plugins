3c5c1c8e5d7b0b4ac349ce1b282790de9190669b
Warilized <anim3ddicted@gmail.com>
2021-07-15 00:40:27 +0700

    Branching eqD_groupedBGM.js

--- Utility Plugins/Variable Array/Utility Plugins/Grouped BGM/eqD_groupedBGM.js
+++ Utility Plugins/Variable Array/Utility Plugins/Grouped BGM/eqD_groupedBGM.js
@@ -11,7 +11,7 @@ var Imported = Imported || {};
 Imported.eqD_variableArray = "0.0.1";
 
 var eqD = eqD || {};
-equalDelight.variableArray = equalDelight.variableArray || {};
+equalDelight.groupedBGM = equalDelight.groupedBGM || {};
 eqD.PluginCommands = eqD.PluginCommands || {};
 
 //=============================================================================
