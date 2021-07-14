36699e16ddf7597a33211029f8b52d7264d89d18
Warilized <anim3ddicted@gmail.com>
2021-07-14 12:25:53 +0700

    Update Temp_pluginTemplate.js

--- Temp_pluginTemplate.js
+++ Temp_pluginTemplate.js
@@ -1,1 +1,40 @@
 
+//=============================================================================
+// <Plugin Name>
+// By <Author>
+// <filename>.js
+// Version: <version>
+// Released under <your license>
+//=============================================================================
+// This is the imported variable, like you use
+
+var Imported = Imported || {};
+Imported["MyPluginName"] = true; // or instead of true, a version string, like "1.0.1"
+
+// This variable will work as your module, like yanfly's YEP, for example
+var MyName = MyName || {};
+
+// Plugin starts here
+(function ($) { 
+    
+    "use strict"; // Forcing strict mode  
+
+// Imports  
+
+// Private variable
+
+// Public variables  
+
+// Private classes  
+
+// Private functions
+
+// Public classes  
+
+// Public functions  
+
+// Export area
+    
+})(MyName); // Do you see the MyName here, that is the value of $ above.
+
+// Plugin ends here
